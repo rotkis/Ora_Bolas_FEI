044c169926352318ee494b7fdbe2c2c5d11fd7ab
primol <78610706+primol@users.noreply.github.com>
2024-05-13 18:06:09 +0000

    Gráficos

---
+++ graficos.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+import math # matematica
+
+plt.title("Trajetória da Bola") 
+plt.xlabel("X") 
+plt.ylabel("Y")
+plt.grid(True)
+plt.plot(x, y)
+
+
+plt.show()
